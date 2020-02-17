3d1bde6a9086181b0a529684a9a4a44c6a1e87da
johannestigges <johannes.tigges@freenet.de>
2020-02-17 19:59:54 +0100

    migrate to angular 9

--- src/app/cors-interceptor.ts
+++ src/app/cors-interceptor.ts
@@ -1,7 +1,9 @@
 import { HttpInterceptor, HttpXsrfTokenExtractor, HttpRequest, HttpHandler, HttpEvent } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { Injectable } from "@angular/core";
 
 
+@Injectable()
 export class CorsInterceptor implements HttpInterceptor {
     constructor(private tokenExtractor: HttpXsrfTokenExtractor) { }
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
