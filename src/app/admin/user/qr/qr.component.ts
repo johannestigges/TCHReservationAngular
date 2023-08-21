125422052bff2131b3875e10519a367846fb3784
johannestigges <johannes.tigges@freenet.de>
2023-08-21 16:50:06 +0200

    insert and update user with qr code

--- src/app/admin/user/qr/qr.component.ts
+++ src/app/admin/user/qr/qr.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { User } from '../user';
 import { UserRole } from '../user-role.enum';
 import { ActivationStatus } from '../activation-status.enum';
@@ -8,13 +8,12 @@ import { Location } from '@angular/common';
 
 @Component({
   selector: 'tch-qr',
-  templateUrl: './qr.component.html',
-  styleUrls: ['./qr.component.scss']
+  templateUrl: './qr.component.html'
 })
-export class QrComponent extends ErrorAware {
-
+export class QrComponent extends ErrorAware implements OnInit {
+  users: User[] = [];
+  activeUsers: User[] = [];
   user: User
-
   qrUrl = '';
 
   constructor(private readonly location: Location, private readonly userService: UserService) {
@@ -22,17 +21,36 @@ export class QrComponent extends ErrorAware {
     this.user = new User(0, '', UserRole.REGISTERED, null, this.createPassword(), ActivationStatus.ACTIVE);
   }
 
+  ngOnInit() {
+      this.userService.getAll().subscribe(users=>{
+      this.users=users;
+      this.activeUsers = this.users.filter(u => this.isActive(u.status));
+      console.log('users', this.users.length, this.activeUsers.length, this.users);
+      });
+  }
 
-  onClick() {
+  private isActive(status) {
+    return 'ACTIVE' === status.toString();
+  }
+
+  onClickNewUser() {
     if (this.user.name) {
-      this.userService.addUser(this.user).subscribe({
-        next: () => this.qrUrl =
-          `${window.location.origin}/#/login?username=${this.user.name}&password=${this.user.password}`,
+      this.clearError();
+        if (this.users.find(u => this.user.name === u.name)) {
+          this.errorMessages.push("Den Benutzer gibt es bereits.");
+          this.qrUrl='';
+          return;
+        }
+        this.userService.addUser(this.user).subscribe({
+        next: () => this.qrUrl = this.generateUrl(),
         error: (error) => this.setError(error)
       });
     } else {
       this.qrUrl = '';
     };
+  }
+
+  onClickNewPassword() {
   }
 
   onClose() {
@@ -44,5 +62,8 @@ export class QrComponent extends ErrorAware {
       .fill("0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz")
       .map(function (x) { return x[Math.floor(Math.random() * x.length)] })
       .join('');
+  }
+  private generateUrl() {
+    return `${window.location.origin}/#/login?username=${this.user.name}&password=${this.user.password}`;
   }
 }
