deee5b1dcd5e592a3376ef4d804c0687a7a1f4d0
johannestigges <johannes.tigges@freenet.de>
2023-09-08 18:39:48 +0200

    update npm and lint fix

--- src/app/admin/systemconfig/systemconfig-form.ts
+++ src/app/admin/systemconfig/systemconfig-form.ts
@@ -1,4 +1,4 @@
-import { FormArray, FormControl, FormGroup, Validators } from "@angular/forms";
+import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 
 export interface SystemconfigForm {
     id: FormControl<number>,
@@ -13,15 +13,15 @@ export interface SystemconfigForm {
 }
 
 export function createSystemConfigForm(): FormGroup<SystemconfigForm> {
-    return new FormGroup({
-        id: new FormControl(0, Validators.required),
-        name: new FormControl('', Validators.required),
-        title: new FormControl('', Validators.required),
-        courts: new FormArray([]),
-        durationUnitInMinutes: new FormControl(60, Validators.required),
-        maxDaysReservationInFuture: new FormControl(0, Validators.required),
-        maxDuration: new FormControl(1, Validators.required),
-        openingHour: new FormControl(8, Validators.required),
-        closingHour: new FormControl(22, Validators.required)
-    });
+	return new FormGroup({
+		id: new FormControl(0, Validators.required),
+		name: new FormControl('', Validators.required),
+		title: new FormControl('', Validators.required),
+		courts: new FormArray([]),
+		durationUnitInMinutes: new FormControl(60, Validators.required),
+		maxDaysReservationInFuture: new FormControl(0, Validators.required),
+		maxDuration: new FormControl(1, Validators.required),
+		openingHour: new FormControl(8, Validators.required),
+		closingHour: new FormControl(22, Validators.required)
+	});
 }
