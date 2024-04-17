b73f275d3d888890d24ee526b4a72fc742db2e29
johannestigges <johannes.tigges@freenet.de>
2024-04-17 21:45:17 +0200

    fixed time options and update package.json

---
+++ src/app/admin/news/news.ts
@@ -0,0 +1,9 @@
+export class News {
+    constructor (
+        public id: number,
+        public subject: string,
+        public url: string,
+        public text: string,
+        public createdAt: number
+    ) {}
+}
---
+++ src/app/admin/news/overview/news-overview.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+import { Location } from '@angular/common';
+import { News } from '../news';
+import { ErrorAware } from 'src/app/util/error/error-aware';
+import { NewsService } from '../news.service';
+import { DateUtil } from 'src/app/util/date/date-util';
+
+@Component({
+  selector: 'tch-news-overview',
+  standalone: false,
+  templateUrl: './news-overview.component.html',
+  styleUrl: './news-overview.component.scss'
+})
+export class NewsOverviewComponent extends ErrorAware implements OnInit {
+
+  newsList: News[] = [];
+
+  constructor(private newsService: NewsService, private location: Location) {
+    super();
+  }
+
+  ngOnInit(): void {
+    this.newsService.getAll().subscribe({
+      next: (data) => this.newsList = data,
+      error: (error) => this.setError(error)
+    });
+  }
+
+  onDelete(id: number) {
+    this.newsService.delete(id).subscribe({
+      next: (data) => this.ngOnInit(),
+      error: (error) => this.setError(error)
+    });
+  }
+
+  cancel() {
+    this.location.back();
+  }
+
+  date(t: number) {
+    return DateUtil.showDateTime(t);
+  }
+}
